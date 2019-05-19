68085bd686a225d2981051a631c7e16d4aa03d5e
csmiguel <miguelcamachosanchez@gmail.com>
2019-05-19 23:20:33 +0100

    models autocorrelation

--- src/03-autocorrelation.R
+++ src/03-autocorrelation.R
@@ -27,9 +27,9 @@ input2 <- "data/intermediate/alpha_diversity.rds"
 div <- readRDS(input2)
 
 #create new variable concatenating Location and elevation
-animals <- animals %>% mutate(Site = as.factor(paste(animals$Location,
+animals <- animals %>% dplyr::mutate(Site = as.factor(paste(animals$Location,
           animals$Transect_altitude, sep = "_")))
-
+#get centroid coordinates for each trapping location
 coords <- animals %>% dplyr::select(Site, Longitude, Latitude) %>%
           plyr::ddply("Site", function(x) {
             x <- dplyr::select(x, Longitude, Latitude)
@@ -43,46 +43,81 @@ coords <- animals %>% dplyr::select(Site, Longitude, Latitude) %>%
         )
 
 #create input for autocorrelation test
-input_autocorr <- left_join(div, coords, by = c("Site", "Site"))
+input_autocorr <- dplyr::left_join(div, coords, by = c("Site", "Site")) %>%
+    dplyr::arrange(Location, Elevation)
 
-# fit model
-full_model <- nlme::gls(Richness ~ Elevation, data = input_autocorr,
-  method = "ML", correlation = nlme::corSpatial(form = ~ x + y))
+#include geometric constraints as a variable
+rmod <- readRDS("data/intermediate/rangemodel.rds")
+# convert results from rangemodel from a list to a dataframe
+rangemodel <-
+seq_along(rmod) %>% plyr::ldply(function(x) {
+  rmod[[x]] %>%
+   select(mean, class) %>% mutate(mt = names(rmod[x]))
+   })
+#calculate the species richness for the closest elevation from rangemodel
+  input_autocorr2 <-
+  input_autocorr %>% plyr::ddply("Location", function(x){
+    mount <- x$Location[1]
+    seq_along(x$Location) %>% sapply(function(y){
+      elev <- x$Elevation[y]
+      rmdel <- rangemodel %>% filter(mt == mount)
+      minv <- abs(rmdel$class - elev)
+      mean_sr <- rmdel[which(minv == sort(minv)[1]), ]$mean %>% mean
+      c(mount, elev, mean_sr)
+    }
+    ) %>% t
+  }
+  ) %>% dplyr::rename(Elevation = "2") %>%
+      dplyr::rename(mde_richness = "3") %>%
+      dplyr::select(-"1") %>%
+  #join to input_autocorr
+dplyr::left_join(input_autocorr, ., by = c("Location", "Elevation"))
 
-# fit null model
-null_model <- nlme::gls(Richness ~ 1, data = input_autocorr,
-  method = "ML", correlation = nlme::corSpatial(form = ~ x + y))
+#I cannot compare them with anova
+#lmer: model to include random effects and spatial autocorrelation
+m1 <- nlme::lme(Richness ~ Elevation + mde_richness, random = ~1 | Location,
+  data = input_autocorr2, correlation = corSpatial(form = ~ x + y),
+  method = "ML")
+m2 <- nlme::lme(Richness ~ Elevation, random = ~1 | Location,
+  data = input_autocorr2, correlation = corSpatial(form = ~ x + y),
+  method = "ML")
+m3 <- nlme::lme(Richness ~ mde_richness, random = ~1 | Location,
+  data = input_autocorr2, correlation = corSpatial(form = ~ x + y),
+  method = "ML")
+m4 <- nlme::lme(Richness ~ 1, random = ~1 | Location,
+  data = input_autocorr2, correlation = corSpatial(form = ~ x + y),
+  method = "ML")
 
-# compare models
-an <- anova(full_model, null_model)
+#Plot results
+all_models <- list(m1 = m1, m2 = m2, m3 = m3, m4 = m4)
 
-# create data for trend line
-pred_data <- data.frame(Elevation = seq(min(input_autocorr$Elevation),
-                                       max(input_autocorr$Elevation),
-                                       length.out = 1000))
-pred_data$Richness <- predict(full_model, newdata = pred_data)
-# plot results
-plot(Richness ~ Elevation, data = input_autocorr)
-lines(Richness ~ Elevation, data = pred_data, col = "red")
+pred <- seq_along(all_models) %>% sapply(function(x){
+  predict(all_models[[x]])
+  }
+) %>% as.data.frame
+names(pred) <- paste0("pred_", names(all_models))
+res <- seq_along(all_models) %>% sapply(function(x){
+  residuals(all_models[[x]])
+  }
+) %>% as.data.frame
+names(res) <- paste0("res_", names(all_models))
+
+results <- cbind(input_autocorr2, pred, res)
 
 #save results to file
-sink("output/autocorrelation.txt")
-  cat("Autocorrelation test\n Full model")
-  summary(full_model)
-  cat("Null model")
-  summary(null_model)
-  cat("ANOVA comparing both models")
-  an
+sink("output/models_alpha_diversity.txt")
+for (i in seq_along(all_models)){
+  cat("\n\n\nNext model\n")
+  cat(names(all_models)[i], "\n\n")
+  print(summary(all_models[[i]]))
+}
+cat("\n\n\nANOVA\n\n\n")
+anova(m1, m2)
+anova(m1, m3)
+anova(m1, m4)
+anova(m2, m4)
+anova(m3, m4)
 sink()
 
-# MLPE model
-  #Jeff Hanson kindly suggested this approach and provided the code.
-    #check reference 10.1002/sim.3429
-  #it is suppose to deal with the non-independence of points from matrix
-  #comparisons as in mantel tests.
-library(ResistanceGA)
-source("src/functions/mlpe.R")
-input3 <- "data/intermediate/bray_curtis_distance.rds"
-bc_b <- readRDS(input3)
-m1 <- mlpe(y = dist(as.matrix(input_autocorr$x, input_autocorr$y)), x = bc_b)
-#issue: interpret output
+#save data
+saveRDS(results, "data/intermediate/models_alpha_diversity.rds")
